4f9e4b70bb75e09d966175627f08d9a4b5ba751b
Zergio200kism <Zergio200kism@gmail.com>
2025-04-16 18:09:05 -0500

    dist

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
-
+app.use(express.static('dist'))
 const requestLogger = (request,response,next) =>{
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
