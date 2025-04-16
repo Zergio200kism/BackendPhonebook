1317761f41708af1e34511a6921fdd7f612fb274
Zergio200kism <Zergio200kism@gmail.com>
2025-04-16 18:01:01 -0500

    process..env.PORT

--- index.js
+++ index.js
@@ -120,5 +120,5 @@ app.post('/api/phonebook',(request,response)=>{
 })
 
 app.use(unknownEndpoint)
-const PORT=3001
+const PORT= process.env.PORT || 3001
 app.listen(PORT,()=>{console.log("Escuchando peticiones...")})
